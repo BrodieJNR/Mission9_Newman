1fe37425764a3a8d01321ffd5388d7350639ac47
BrodieJNR <Brodiej_Newman@hotmail.com>
2026-03-03 20:17:17 -0700

    Add comments and clean up Heading.tsx
    Added a description paragraph to the Heading component.

--- frontend/src/components/Heading.tsx
+++ frontend/src/components/Heading.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+// Heading component that displays the site title and description
+const Heading: React.FC = () => {
+  return (
+    <header className="heading">
+      <h1>March Madness NCAA Basketball Teams</h1>
+      <p>Browse information about all NCAA college basketball teams.</p>
+    </header>
+  );
+};
+
+export default Heading;
 const Heading: React.FC = () => {
   return (
     <header className="heading">
