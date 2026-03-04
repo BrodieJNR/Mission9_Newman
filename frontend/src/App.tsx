bb9e13ec31a45c9100a51a3b4eb63fe01bba63fa
BrodieJNR <Brodiej_Newman@hotmail.com>
2026-03-03 20:16:46 -0700

    Add comments and clean up App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,19 @@ import './App.css';
 import Heading from './components/Heading.tsx';
 import TeamsList from './components/TeamsList.tsx';
 
+// Main App component that serves as the root of the application
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+      <TeamsList />
+    </div>
+  );
+}
+
+export default App;import Heading from './components/Heading.tsx';
+import TeamsList from './components/TeamsList.tsx';
+
 function App() {
   return (
     <div className="App">
